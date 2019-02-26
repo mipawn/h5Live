e61467b2416c810b7392b694af5b6d28dbbf8de5
mario <3382189838@qq.com>
2019-02-26 17:19:40 +0800

    comments

--- src/pages/components/footerbar.vue
+++ src/pages/components/footerbar.vue
@@ -3,7 +3,10 @@
     <div class="main-position">
       <div class="main">
         <div :class="'left' + (isWriting ? ' writing' : '')">
-          <div class="content" ref="content" contenteditable="true" @blur="contentBlur"
+          <!-- 编辑器元素 -->
+          <div class="content" ref="content"
+            contenteditable="true"
+            @blur="contentBlur"
             @click="contentFoucs">
           </div>
           <div class="emoji" @click="showPackage">
@@ -11,14 +14,15 @@
           </div>
           <div class="send" @click="sendComment">发送</div>
         </div>
-        <div class="emoji-package" v-show="show">
+        <!-- 表情包 -->
+        <div class="emoji-package" v-show="show" @click="addImage">
           <div class="page1">
             <img :src="'https://res.wx.qq.com/mpres/htmledition/images/icon/emotion/' + n + '.gif'"
-              alt="" v-for="n in 21" :key="n" @click="addImage" />
+              alt="" v-for="n in 21" :key="n" />
           </div>
           <div class="page2">
             <img :src="'https://res.wx.qq.com/mpres/htmledition/images/icon/emotion/' + (n + 21) + '.gif'"
-              alt="" v-for="n in 21" :key="n" @click="addImage" />
+              alt="" v-for="n in 21" :key="n" />
           </div>
         </div>
         <div class="center" @click="startWrite" v-show="!isWriting">
@@ -53,53 +57,74 @@ export default {
     }
   },
   methods: {
-    showPackage () {
-      this.show = !this.show
-      this.setCursor()
-    },
     contentBlur () {
+      /**
+       * 每次焦点移出都要保存一次当前 range ，后面好放回来
+       * 由于输入框以外的点击都会使输入框失去焦点，不知道会有什么操作，故勤保存
+       */
       this.range = this.selection.getRangeAt(0)
     },
     startWrite () {
       this.isWriting = true
-      // document.body.style.position = 'fixed'
-      setTimeout(() => {
-        document.documentElement.scrollTop = window.innerHeight
-      }, 1000)
       if (this.content) {
+        // 如果focus前有内容， 光标设置到之前保存的位置 setCursor
         this.setCursor()
       } else {
+        // 开始评论，输入框自动聚焦
         this.$refs.content.focus()
         this.contentFoucs()
       }
     },
     contentFoucs (e) {
+      /**
+       * range更新到点击的位置
+       * 点到图片就根据点击位置和图片大小设置一个合理的位置(前或后)
+       */
       let node = e ? e.target : {}
       this.range = this.selection.getRangeAt(0)
       if (node.tagName === 'IMG') {
         this.setCursor(node, e.offsetX < node.width / 2)
       }
     },
+    showPackage () {
+      this.show = !this.show
+      this.setCursor()
+    },
     addImage (e) {
-      let node = e.target.cloneNode(true)
-      this.range.insertNode(node)
-      this.setCursor(node, false)
+      /** 
+       * 点击emoji图片后，复制节点添到当前range里并设置光标和更新range 
+       * 若是点到其他地方则设置到之前的位置
+       */
+      if (e.target.tagName === 'IMG') {
+        let node = e.target.cloneNode(true)
+        this.range.insertNode(node)
+        this.setCursor(node, false)
+      } else {
+        this.setCursor()
+      }
     },
     setCursor (node, before) {
+      /**
+       * node 为传入的节点，不传则foucs到之前保存的位置
+       * before 控制折叠方向
+       */
       if (node) {
+        // 需要新建一个range来添加内容
         let range = document.createRange()
-        range.selectNodeContents(node)
         range.selectNode(node)
-        range.collapse(false)
+        range.collapse(!!before)
         this.selection.removeAllRanges()
         this.selection.addRange(range)
-        this.range = this.selection.getRangeAt(0)
+        // 更新 range
+        this.range = range
       } else {
         this.selection.removeAllRanges()
+        // 使用之前的
         this.selection.addRange(this.range)
       }
     },
     footerBlur () {
+      // 退出输入状态，保存内容
       this.show = false
       this.isWriting = false
       this.content = this.$refs.content.innerHTML
@@ -116,11 +141,10 @@ export default {
     doCollect () {
     },
     doShare () {
-      if (window.PalauAPI) {
-      }
     }
   },
   mounted () {
+    // 先得到selection，并创建一个range
     this.selection = document.getSelection()
     this.range = document.createRange()
   }
@@ -157,12 +181,12 @@ div.footer-hide
         border-top 1px solid #ddd
         position absolute
         right 0
-        // bottom calc(100% + 2px)
         bottom 100%
         background #ffffff
         width 100vw
         height 2.4rem
         overflow-x scroll
+        user-select none
         >div
           height 100%
           display grid
@@ -208,6 +232,7 @@ div.footer-hide
           line-height 1.4
           border 1px solid #ddd
           border-radius 3px
+          word-break break-all
           >img
             width 1.4em
       .center
